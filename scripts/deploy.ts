352d7c51fd0f8e8724ea59a74da211579aebbffd
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-10-08 16:54:05 +0100

    file restructure

---
+++ scripts/deploy.ts
@@ -0,0 +1,29 @@
+import hre from "hardhat";
+
+type Time = number;
+
+async function sleep(ms: Time) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  console.log("-----------------------------");
+  console.log("Deploying whitelist contract");
+
+  const whitelistContract = await hre.ethers.deployContract("Whitelist", [50]);
+  await whitelistContract.waitForDeployment();
+
+  console.log(`Whitelist contract deployed at: ${whitelistContract.target}`);
+
+  await sleep(30 * 1000);
+
+  await hre.run("verify:verify", {
+    address: whitelistContract.target,
+    constructorArguments: [50],
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
